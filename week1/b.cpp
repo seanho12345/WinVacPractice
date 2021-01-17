282c6142ca7618544abed7ae1341101bf31ed75c
Vonernue <39663381+seanho12345@users.noreply.github.com>
2021-01-17 19:33:26 +0800

    B upload

--- week1/b.cpp
+++ week1/b.cpp
@@ -1,2 +1,25 @@
 #include <iostream>
 using namespace std;
+int main(){
+    while(true){
+        int m1,h1,m2,h2;
+        int ans = 0;
+        cin>>h1>>m1>>h2>>m2;
+        if(m1==0&&h1==0&&m2==0&&h2==0){
+            break;
+        }
+        if(h2<h1){
+            h2+=24;
+        }
+        if(h2==h1 && m2<m1){
+            h2+=24;
+        }
+        if(m2<m1){
+            h2--;
+            m2+=60;
+        }
+        ans = (h2-h1)*60+m2-m1;
+        cout<<ans<<"\n";
+    }
+    return 0;
+}
